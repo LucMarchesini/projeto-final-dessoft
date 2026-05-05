fc2443939908d2d4ce4d25806fd2d6f7458f3b0d
Lucas Marchesini <lucassmarchesini@gmail.com>
2026-05-05 10:10:44 -0300

    Sprites de background e jogador 1 carregados

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 # --- Inicialização ---
 pygame.init()
 
@@ -12,9 +12,14 @@ pygame.display.set_caption("Corner Fight")
 # --- Clock (controla FPS) ---
 clock = pygame.time.Clock()
 FPS = 60
-
+# --- Definição para o jogo continuar
 rodando = True
 
+# --- Inicialização dos assets (sem animações por enquanto)
+assets = {}
+assets['background'] = pygame.image.load('sprites/background').convert()
+assets['jogador_um'] = pygame.image.load('sprites\Brawler-Girl\Idle').convert_alpha()
+assets['jogador_um'] = pygame.transform.scale(assets['jogador_um'], (20, 100))
 # --- Cores ---
 cores={
     'Preto': (0, 0, 0),
