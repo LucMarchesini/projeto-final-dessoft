c4f1f7e1da90af3039b26c55dad44107ddbf5ca9
Lucas Marchesini <lucassmarchesini@gmail.com>
2026-05-05 08:49:41 -0300

    Atualização em MAIN, finalização mais polida, alteração do método de atuaização

--- src/main.py
+++ src/main.py
@@ -7,11 +7,13 @@ pygame.init()
 largura_janela = 1280
 altura_janela = 720
 tela = pygame.display.set_mode((largura_janela, altura_janela))
-pygame.display.set_caption("Nome do Jogo")
+pygame.display.set_caption("Corner Fight")
 
 # --- Clock (controla FPS) ---
 clock = pygame.time.Clock()
 FPS = 60
+
+rodando = True
 
 # --- Cores ---
 cores={
@@ -20,17 +22,20 @@ cores={
 }
 
 # --- Loop principal ---
-while True:
+while rodando:
 
     # 1. Eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
-            pygame.quit()
+            rodando = False
 
     # 2. Update (lógica do jogo vai aqui)
 
     # 3. Draw
     tela.fill(cores['Preto'])
 
-    pygame.display.flip()
+    pygame.display.update()
     clock.tick(FPS)
+
+# --- Finalização ---
+pygame.quit()
