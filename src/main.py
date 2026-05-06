cd4bf1e281e6d3bed763d541fa78875976959ede
Thiago Henrique <thiago.santos.insper@gmail.com>
2026-05-06 18:41:20 -0300

    botões tela inicial funcionando com sprites de teste

--- src/main.py
+++ src/main.py
@@ -23,22 +23,47 @@ assets['background'] = pygame.transform.scale(assets['background'], (largura_jan
 assets['jogador_um'] = pygame.image.load(r'sprites/Brawler-Girl/Idle/idle1.png').convert_alpha()
 assets['jogador_um'] = pygame.transform.scale(assets['jogador_um'], (450, 300))
 
+assets['tela_ranking'] = pygame.image.load('sprites/tela_ranking.png').convert()
+assets['tela_ranking'] = pygame.transform.scale(assets['tela_ranking'], (largura_janela, altura_janela))
+
+assets['tela_configuracoes'] = pygame.image.load('sprites/tela_configuracoes.png').convert()
+assets['tela_configuracoes'] = pygame.transform.scale(assets['tela_configuracoes'], (largura_janela, altura_janela))
+
+assets['tela_personagem'] = pygame.image.load('sprites/tela_personagem.png').convert()
+assets['tela_personagem'] = pygame.transform.scale(assets['tela_personagem'], (largura_janela, altura_janela))
+
 # --- Fontes ---
 font_title = pygame.font.SysFont("Arial", 64, bold=True)
 font_sub   = pygame.font.SysFont("Arial", 28)
 
 # --- Funções (recebem assets como parâmetro) ---
 def tela_inicial(assets):
-    botao_jogar = pygame.Rect(360, 290, 560, 80)
+    botao_jogar = pygame.Rect(360, 285, 560, 80)
+    botao_ranking = pygame.Rect(360, 375, 560, 80)
+    botao_configuracoes = pygame.Rect(360, 465, 560, 80)
+    botao_sair = pygame.Rect(360, 555, 560, 80)
+
     while True:
         tela.blit(assets['inicial'], (0, 0))
+
+        pygame.draw.rect(tela, (0, 255, 0), botao_jogar, 3)                     # exibe a heatbox do botão de play 
+        pygame.draw.rect(tela, (0, 255, 0), botao_ranking, 3)                   # exibe a heatbox do botão de ranking
+        pygame.draw.rect(tela, (0, 255, 0), botao_configuracoes, 3)             # exibe a heatbox do botão de configurações
+        pygame.draw.rect(tela, (0, 255, 0), botao_sair, 3)                      # exibe a heatbox do botão de sair
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if botao_jogar.collidepoint(event.pos):
-                        return
+                        loop_jogo(assets)
+                    elif botao_ranking.collidepoint(event.pos):
+                        tela_ranking(assets)
+                    elif botao_configuracoes.collidepoint(event.pos):
+                        tela_configuracoes(assets)
+                    elif botao_sair.collidepoint(event.pos):
+                        pygame.quit()
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -85,7 +110,39 @@ def loop_jogo(assets):
         
         pygame.display.update()
 
+def tela_ranking(assets):
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+        
+        tela.blit(assets['tela_ranking'], (0, 0))
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+def tela_configuracoes(assets):
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+        
+        tela.blit(assets['tela_configuracoes'], (0, 0))
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+def tela_personagem(assets):
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+
+        tela.blit(assets['tela_personagem'], (0, 0))
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
 # --- Execução ---
 tela_inicial(assets)
-loop_jogo(assets)
 pygame.quit()
