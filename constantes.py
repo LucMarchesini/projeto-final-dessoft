35086932b4e466cf7902955466c4789f75d8fbb1
Thiago Henrique <thiago.santos.insper@gmail.com>
2026-05-07 00:18:07 -0300

    cria conteúdo, de fato, do arquivo das constantes

---
+++ constantes.py
@@ -0,0 +1,32 @@
+# --- Janela ---
+LARGURA_JANELA = 1280
+ALTURA_JANELA = 720
+FPS = 60
+TITULO = "Corner Fight"
+
+# --- Física ---
+GRAVIDADE = 0.8
+IMPULSO_PULO = -15
+VELOCIDADE_JOGADOR = 5
+Y_CHAO = 390
+
+# --- Cores ---
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+VERDE = (0, 255, 0)
+VERMELHO = (255, 0, 0)
+
+# --- Jogador 1 (setas + espaço) ---
+P1_DIREITA = "RIGHT"
+P1_ESQUERDA = "LEFT"
+P1_PULO = "UP"
+P1_SOCO = "SPACE"
+
+# --- Jogador 2 (setas + espaço) ---
+P1_DIREITA = "D"
+P1_ESQUERDA = "A"
+P1_PULO = "W"
+P1_SOCO = "F"
+
+# --- Debug ---
+DEBUG = True
