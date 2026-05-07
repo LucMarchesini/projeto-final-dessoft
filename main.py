4c9aebc7e07b2c0edb6c684d49ede09088163769
Thiago Henrique <thiago.santos.insper@gmail.com>
2026-05-07 00:05:14 -0300

    Reorganiza pastas do projeto e carrega assets ausentes

--- main.py
+++ main.py
@@ -3,38 +3,46 @@ import random
 
 pygame.init()
 
-largura_janela = 1280
-altura_janela = 720
-tela = pygame.display.set_mode((largura_janela, altura_janela))
-pygame.display.set_caption("Corner Fight")
+LARGURA_JANELA = 1280
+ALTURA_JANELA = 720
 
-clock = pygame.time.Clock()
-FPS = 60
+tela = pygame.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))                 # Cria a janela do jogo com o tamanho definido dentro dos parênteses
+pygame.display.set_caption("Corner Fight")                                      # Cria o título da janela
+
+clock = pygame.time.Clock()                                                     # Cria um objeto de controle de tempo, usado para limitar os quadros por segundo
+FPS = 60                                                                        # Define a variável FPS
 
 # --- Carrega todos os assets uma vez só ---
 assets = {}
 
-assets['inicial'] = pygame.image.load('sprites/tela_inicial.png').convert()
-assets['inicial'] = pygame.transform.scale(assets['inicial'], (largura_janela, altura_janela))
+assets['inicial'] = pygame.image.load('assets/sprites/tela_inicial.png').convert()
+assets['inicial'] = pygame.transform.scale(assets['inicial'], (LARGURA_JANELA, ALTURA_JANELA))
 
-assets['background'] = pygame.image.load('sprites/background.png').convert()
-assets['background'] = pygame.transform.scale(assets['background'], (largura_janela, altura_janela))
+assets['background'] = pygame.image.load('assets/sprites/background.png').convert()
+assets['background'] = pygame.transform.scale(assets['background'], (LARGURA_JANELA, ALTURA_JANELA))
 
-assets['jogador_um'] = pygame.image.load(r'sprites/Brawler-Girl/Idle/idle1.png').convert_alpha()
+assets['jogador_um'] = pygame.image.load(r'assets/sprites/Brawler-Girl/Idle/idle1.png').convert_alpha()
 assets['jogador_um'] = pygame.transform.scale(assets['jogador_um'], (450, 300))
 
-assets['tela_ranking'] = pygame.image.load('sprites/tela_ranking.png').convert()
-assets['tela_ranking'] = pygame.transform.scale(assets['tela_ranking'], (largura_janela, altura_janela))
+assets['jogador_dois'] = assets['jogador_um']
 
-assets['tela_configuracoes'] = pygame.image.load('sprites/tela_configuracoes.png').convert()
-assets['tela_configuracoes'] = pygame.transform.scale(assets['tela_configuracoes'], (largura_janela, altura_janela))
+assets['jogador_um_soco'] = pygame.image.load(r'assets/sprites/Brawler-Girl/Jab/jab3.png').convert_alpha()
+assets['jogador_um_soco'] = pygame.transform.scale(assets['jogador_um_soco'], (450, 300))
 
-assets['tela_personagem'] = pygame.image.load('sprites/tela_personagem.png').convert()
-assets['tela_personagem'] = pygame.transform.scale(assets['tela_personagem'], (largura_janela, altura_janela))
+assets['jogador_dois_soco'] = assets['jogador_um_soco']
+
+assets['tela_ranking'] = pygame.image.load('assets/sprites/tela_ranking.png').convert()
+assets['tela_ranking'] = pygame.transform.scale(assets['tela_ranking'], (LARGURA_JANELA, ALTURA_JANELA))
+
+assets['tela_configuracoes'] = pygame.image.load('assets/sprites/tela_configuracoes.png').convert()
+assets['tela_configuracoes'] = pygame.transform.scale(assets['tela_configuracoes'], (LARGURA_JANELA, ALTURA_JANELA))
+
+assets['tela_personagem'] = pygame.image.load('assets/sprites/tela_personagem.png').convert()
+assets['tela_personagem'] = pygame.transform.scale(assets['tela_personagem'], (LARGURA_JANELA, ALTURA_JANELA))
 
 # --- Fontes ---
 font_title = pygame.font.SysFont("Arial", 64, bold=True)
-font_sub   = pygame.font.SysFont("Arial", 28)
+font_sub = pygame.font.SysFont("Arial", 28)
 
 # --- Funções (recebem assets como parâmetro) ---
 def tela_inicial(assets):
@@ -46,10 +54,10 @@ def tela_inicial(assets):
     while True:
         tela.blit(assets['inicial'], (0, 0))
 
-        pygame.draw.rect(tela, (0, 255, 0), botao_jogar, 3)                     # exibe a heatbox do botão de play 
-        pygame.draw.rect(tela, (0, 255, 0), botao_ranking, 3)                   # exibe a heatbox do botão de ranking
-        pygame.draw.rect(tela, (0, 255, 0), botao_configuracoes, 3)             # exibe a heatbox do botão de configurações
-        pygame.draw.rect(tela, (0, 255, 0), botao_sair, 3)                      # exibe a heatbox do botão de sair
+        pygame.draw.rect(tela, (0, 255, 0), botao_jogar, 3)                     # exibe a hitbox do botão de play 
+        pygame.draw.rect(tela, (0, 255, 0), botao_ranking, 3)                   # exibe a hitbox do botão de ranking
+        pygame.draw.rect(tela, (0, 255, 0), botao_configuracoes, 3)             # exibe a hitbox do botão de configurações
+        pygame.draw.rect(tela, (0, 255, 0), botao_sair, 3)                      # exibe a hitbox do botão de sair
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
