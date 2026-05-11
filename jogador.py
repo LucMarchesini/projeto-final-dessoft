0e76978fcff87d247dc454b4a9944469fd4b838b
Lucas Marchesini <lucassmarchesini@gmail.com>
2026-05-11 16:15:54 -0300

    Jogador atualizado para ele ter uma função que desenha todos os sprites dele

--- jogador.py
+++ jogador.py
@@ -1,17 +1,14 @@
 import pygame
 import constantes as C
-from assets import carregar_assets
-
-assets = carregar_assets()
 
 class Jogador:
-    def __init__(self, x, y, controles, assets, estado, tipo):
+    def __init__(self, x, y, controles, assets, tipo):
         self.x = x
         self.y = y
 
         self.vel_y = 0
-        self.no_chao = True                 # futuramente isso será removido
-        self.estado = estado                # pulando, socando, no chão, pulando + socando etc
+        self.no_chao = True
+        self.socando = False
 
         self.controles = controles
         self.assets = assets
@@ -20,58 +17,39 @@ class Jogador:
     def atualizar(self, teclas):
         delta_x = 0
 
-        # Movimento Horizontal
         if teclas[self.controles["direita"]]:
             delta_x += C.VELOCIDADE
         if teclas[self.controles["esquerda"]]:
             delta_x -= C.VELOCIDADE
-        
-        # Pulo
+
         if teclas[self.controles["pulo"]] and self.no_chao:
             self.vel_y = C.IMPULSO_PULO
             self.no_chao = False
-        
-        # Soco
+
         self.socando = teclas[self.controles["soco"]]
 
-        # Gravidade
         self.vel_y += C.GRAVIDADE
-
-        # Movimento
-        self.x += self.delta_x
+        self.x += delta_x
         self.y += self.vel_y
-
-        # Colisão com o chão
 
         if self.y >= C.Y_CHAO:
             self.y = C.Y_CHAO
             self.vel_y = 0
             self.no_chao = True
-    
+
     def get_hitbox_jogador(self):
         offset = C.P1_HITBOX_OFFSET if self.tipo == "um" else C.P2_HITBOX_OFFSET
         size = C.P1_HITBOX_SIZE if self.tipo == "um" else C.P2_HITBOX_SIZE
-
-        return pygame.Rect(
-            self.x + offset[0],
-            self.y + offset[1],
-            size[0],
-            size[1]
-        )
+        return pygame.Rect(self.x + offset[0], self.y + offset[1], size[0], size[1])
 
     def get_hitbox_soco(self):
         offset = C.P1_SOCO_OFFSET if self.tipo == "um" else C.P2_SOCO_OFFSET
         size = C.P1_SOCO_SIZE if self.tipo == "um" else C.P2_SOCO_SIZE
+        return pygame.Rect(self.x + offset[0], self.y + offset[1], size[0], size[1])
 
-        return pygame.Rect(
-            self.x + offset[0],
-            self.y + offset[1],
-            size[0],
-            size[1]
-        )
-    
     def desenhar(self, tela):
-        sprite = assets["personagens"][self.estado]
+        if self.socando:
+            sprite = self.assets['jogador_um_soco'] if self.tipo == "um" else self.assets['jogador_dois_soco']
+        else:
+            sprite = self.assets['jogador_um'] if self.tipo == "um" else self.assets['jogador_dois']
         tela.blit(sprite, (self.x, self.y))
-    
-    
